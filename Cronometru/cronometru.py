0fe76ea68c2886fc8dae26c40df7c6c411991121
Andrei <burcus.andreisilviu22@gmail.com>
2021-12-19 14:37:05 +0200

    Cronometru si sunet + ora/minute/secunde impuse

--- Cronometru/cronometru.py
+++ Cronometru/cronometru.py
@@ -1,6 +1,6 @@
-
 import winsound
 import time
+from datetime import datetime
 
 def sound():
     winsound.Beep(1000, 1000)
@@ -16,7 +16,31 @@ def countdown(t):
     print('Enough for today!!')
     sound()
 
-t = input("Enter the time in seconds: ")
+def rest_calculator():
+    now = datetime.now()
 
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+    a = int(input("La ce ora doriti sa finalizati activitatea?"))
+    b = int(input("La ce minut al orei doriti sa finalizati activitatea?"))
+    c = int(input("La ce secunda al minutului doriti sa finalizati activitatea?"))
+    rest_total = 0
+    rest_ora_secunde = 0
+    rest_minute_secunde = 0
+    rest_secunde = 0
+
+    if a != int(now.strftime("%H")):
+        rest_ora = a - int(now.strftime("%H"))
+        rest_ora_secunde = rest_ora * 3600
+    if b != int(now.strftime("%M")):
+        rest_minute = b - int(now.strftime("%H"))
+        rest_minute_secunde = rest_minute * 60
+    if c != int(now.strftime("%M")):
+        rest_secunde = c- int(now.strftime("%S"))
+    rest_total = rest_ora_secunde + rest_minute_secunde + rest_secunde
+    return rest_total
+
+t = rest_calculator()
 
 countdown(int(t))
