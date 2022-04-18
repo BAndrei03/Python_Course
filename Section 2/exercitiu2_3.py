6da3b206c07a472894459341c2ef979e85cdfc45
Andrei <burcus.andreisilviu22@gmail.com>
2022-04-18 11:43:40 +0300

    Cronometru si sunet + ora/minute/secunde impuse

--- Section 2/exercitiu2_3.py
+++ Section 2/exercitiu2_3.py
@@ -4,11 +4,11 @@ age = input("What is your current age?")
 
 #Write your code below this line 👇
 
-age_int= int(age)
+age_int = int(age)
 years = 90 - age_int
 days = years * 365
 months = years * 12
 weeks = years * 42
 
-message= f"you have {days} days, {months} months or {weeks} weeks to live until 90 years"
+message = f"you have {days} days, {months} months or {weeks} weeks to live until 90 years"
 print(message)
